2fe011f8c615c6fb5f5c91d636b817c0620b7c76
yurimlee <ylee981352@gmail.com>
2022-04-01 22:33:53 +0900

    [#1] longestCommonPrefix (수정 및 추가)

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -1,10 +1,15 @@
 function longestCommonPrefix(strs) {
   if (!strs.length) return '';
-  
+  // 문자열 길이가 아니면 ' ' 을 리턴하고
   for (let i = 0; i < strs[0].length; i++) {
+    // 0번째 줄의 배열 갯수 보다 작으면 
     for (let str of strs) {
+      // str: 위 반복에 서로 다른 속성값이 str에 저장 
+      // strs : 반복되는 열거가능한 속성이 있는 객체
+      // 배열 strs 값이 str로 넘어감 (순환)
       if (str[i] !== strs[0][i]) {
         return str.slice(0, i);
+        // 0번째부터 i까지 자르기 그리고 리턴 
       }
     }
   }
@@ -13,14 +18,3 @@ function longestCommonPrefix(strs) {
 }
 
 
-// function longestCommonPrefix(strs) {
-//   ans = []
-//   for (let i in strs[0]){
-//     for (let in strs){
-//     if (let <= i != let[i]);
-//     return ''
-//     ans.append(ch)
-//     return ans
-//   }
-// }
-// }
