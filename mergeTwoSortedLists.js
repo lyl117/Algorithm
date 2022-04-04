fbadbc5e47c2426c4c95504b267dbf1c163f5f15
yurimlee <ylee981352@gmail.com>
2022-04-04 17:56:49 +0900

    [#1] mergeTwoSortedLists

--- mergeTwoSortedLists.js
+++ mergeTwoSortedLists.js
@@ -1,10 +1,17 @@
-var mergeTwoLists = function(l1, l2) {        
-  if (!l1 && !l2)
-      return null;
-  if (!l1)
+const mergeTwoLists = function(l1, l2) { 
+  if (!l1) return l2; //l1이 아니면 l2 리턴
+  if (!l2) return l1; //l2가 아니면 l1 리턴
+  
+  if(l1.val < l2.val) { 
+    //l1값이 l2값보다 작으면
+      l1.next = mergeTwoLists(l1.next, l2);
+      // li 다음 l2가 나와야하고
+      return l1; 
+      // 그리고 나서 합쳐진 값인, l1이 리턴
+  } else { 
+      l2.next = mergeTwoLists(l1, l2.next); 
+      // 아니면, l2다음에 l1이 나온다.
       return l2;
-  if (!l2)
-      return l1;
-  return (!l2 || l1.val < l2.val) ? 
-      {...l1, next: mergeTwoLists(l1.next, l2)}: {...l2, next: mergeTwoLists(l1, l2.next)};
+      // 그리고 합쳐진 l2를 리턴
+  } 
 };
