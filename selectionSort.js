8a1c18c1820e56d5ee8b189cfaafc52487ab56bb
lyl117 <ylee981352@gmail.com>
2022-04-20 12:50:36 +0900

    [#2] SelectionSort 수정

--- selectionSort.js
+++ selectionSort.js
@@ -1,8 +1,8 @@
-
+//선택정렬
 for (let i =0; i < array.length -1; i++){
-  let mainIndex = i;
+  let minIndex = i;
    for(let j =i; j < array.length; j++ ){
-    if(array[j] < array[minINdex]){
+    if(array[j] < array[minIndex]){
       minIndex = j;
     }
    }
